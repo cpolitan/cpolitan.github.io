9665e222d45297fd932cbc2d4fe504eded052a6f
Bagus Cahyono <bagusbudy@gmail.com>
2019-02-06 01:07:34 +0700

    deploy

---
+++ precache-manifest.e56e897b34bd82dd55fb3231377528dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0ac1e9bc7a3526c7e5c",
+    "url": "/static/js/main.d0ac1e9b.chunk.js"
+  },
+  {
+    "revision": "64013b2a3019bb34777c",
+    "url": "/static/js/1.64013b2a.chunk.js"
+  },
+  {
+    "revision": "d0ac1e9bc7a3526c7e5c",
+    "url": "/static/css/main.3be37c57.chunk.css"
+  },
+  {
+    "revision": "48a727d49609b47c08f791910856b0e1",
+    "url": "/index.html"
+  }
+];
